2f81ba5bda0514c52425993418dd9618a97f61cc
Bodhisatwya <banerjeecoder777@gmail.com>
2026-09-17 04:44:10 +0530

    added parameterized constructor in SavingsAccount.java file and called the super constructor inside it

---
+++ src/jarvis/SavingsAccount.java
@@ -0,0 +1,9 @@
+package jarvis;
+
+public class SavingsAccount extends Account {
+	
+	public SavingsAccount(int accountId, double balance, Customer customer) {
+		super(accountId, balance, customer);
+	}
+
+}
