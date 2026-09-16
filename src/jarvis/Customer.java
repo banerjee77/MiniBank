b4f0e4c78210349d1607289ea4e2a8a1185aabe3
Bodhisatwya <banerjeecoder777@gmail.com>
2026-09-17 02:27:19 +0530

    added parameterized constructor in Customer.java to initialize instance fields

--- src/jarvis/Customer.java
+++ src/jarvis/Customer.java
@@ -5,5 +5,11 @@ public class Customer {
 	int customerId;
 	String customerName;
 	String customerEmail;
+	
+	public Customer(int customerId, String customerName, String customerEmail) {
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.customerEmail = customerEmail;
+	}
 
 }
