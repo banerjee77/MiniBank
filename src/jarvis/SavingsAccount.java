b87d44d424fac2b77d75919afd898fc1ae549a31
Bodhisatwya <banerjeecoder777@gmail.com>
2026-09-17 23:37:20 +0530

    created method inside SavingsAccount to calculate interest

--- src/jarvis/SavingsAccount.java
+++ src/jarvis/SavingsAccount.java
@@ -5,5 +5,15 @@ public class SavingsAccount extends Account {
 	public SavingsAccount(int accountId, double balance, Customer customer) {
 		super(accountId, balance, customer);
 	}
+	
+//	Creating the interest calculator method
+	public double calculateInterest(double rate) {
+		double interest = (getBalance() * rate) / 100;
+		
+		return interest;
+	}
 
 }
+
+
+
