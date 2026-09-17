ac846f1fb7f1edb21e8df49cb73bae9f59bd6b23
Bodhisatwya <banerjeecoder777@gmail.com>
2026-09-18 00:42:06 +0530

    added private instance field and adjusted the constructor in CurrentAccount class

--- src/jarvis/CurrentAccount.java
+++ src/jarvis/CurrentAccount.java
@@ -2,8 +2,12 @@ package jarvis;
 
 public class CurrentAccount extends Account {
 	
-	public CurrentAccount(int accountId, double balance, Customer customer) {
+	private double overDraftAmount;
+	
+	public CurrentAccount(int accountId, double balance, Customer customer, double overDraftAmount) {
 		super(accountId, balance, customer);
+		
+		this.overDraftAmount = overDraftAmount;
 	}
 	
 	@Override
